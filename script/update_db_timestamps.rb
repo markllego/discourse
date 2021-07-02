92b37a6f861f791be5d35932a83c0c455ce6e13d
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2021-07-02 18:59:17 +0400

    WIP

--- script/update_db_timestamps.rb
+++ script/update_db_timestamps.rb
@@ -1,23 +1,52 @@
 # frozen_string_literal: true
 require "pg"
+require "yaml"
 
-def update_timestamps_sql
-  <<~SQL
-    UPDATE table1
-    SET column_3 = column_3 - INTERVAL '1440 minute'
-  SQL
+class TimestampsUpdater
+  def initialize
+    @raw_connection = PG.connect(
+      host: 'localhost',
+      port: 5432,
+      dbname: 'timestamps_moving_testing',
+      user: 'postgres',
+      password: '')
+  end
+
+  def update
+    puts "starting..."
+    columns = all_columns_of_type("timestamp with time zone")
+    columns.map do |row|
+      puts row.class
+      puts row
+    end
+    update_timestamps("table1", "column_1", 1440)
+    puts "done"
+  end
+
+  def self.update
+    TimestampsUpdater.new.update
+  end
+
+  private
+
+  def all_columns_of_type(data_type)
+    table_schema = 'public'
+    sql = <<~SQL
+      SELECT column_name, table_name
+      FROM information_schema.columns
+      WHERE table_schema = '#{table_schema}'
+        AND data_type = '#{data_type}'
+    SQL
+    @raw_connection.exec(sql)
+  end
+
+  def update_timestamps(table_name, column_name, minutes)
+    sql = <<~SQL
+      UPDATE #{table_name}
+      SET #{column_name} = #{column_name} - INTERVAL '#{minutes} minute'
+    SQL
+    @raw_connection.exec(sql)
+  end
 end
 
-puts "starting..."
-
-raw_connection = PG.connect(
-  host: 'localhost',
-  port: 5432,
-  dbname: 'timestamps_moving_testing',
-  user: 'postgres',
-  password: '')
-
-sql = update_timestamps_sql
-raw_connection.exec(sql)
-
-puts "done"
+TimestampsUpdater.update
